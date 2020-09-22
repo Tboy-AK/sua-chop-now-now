d956c310a8ca1458d345b8f5ce9db0596d09ac43
dreamdeveloper002 <bkosemani1@gmail.com>
2020-09-22 14:08:48 +0100

    ft(user-interface):[Finishes #5]

---
+++ UI/script.js
@@ -0,0 +1,18 @@
+const button = document.querySelector('.find');
+
+
+
+
+button.addEventListener('click', displayResults);
+
+
+
+function  displayResults(e) {
+
+  const distance = document.querySelector('.distance');
+  const address = document.querySelector('.address');
+  
+
+  
+  e.preventDefault();
+}
